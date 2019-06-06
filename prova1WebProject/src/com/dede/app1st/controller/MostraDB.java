a8ebffb39bf7ae74ac31cb1b6848c2102f0aa880
Utente <Utente@DESKTOP-VLPD86O>
2019-06-06 15:13:35 +0200

    lala 15:15

--- prova1WebProject/src/com/dede/app1st/controller/MostraDB.java
+++ prova1WebProject/src/com/dede/app1st/controller/MostraDB.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AddUser
  */
-@WebServlet("/CreateUser.do" )
+@WebServlet("/Lista.do" )
 public class MostraDB extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
